fe7b4923084e1e64b7166d3507ef0cced91daad6
sensix <simone.sensidoni@bitapp.it>
2014-02-17 10:16:01 +0000

    sistemata pagina progetto

--- template.php
+++ template.php
@@ -3,3 +3,13 @@
 /**
  * @file template.php
  */
+
+function philoboot_preprocess_page(&$vars) {
+// custom content type page template
+  // Renders a new page template to the list of templates used if it exists
+  if (isset($vars['node']->type)) {
+// This code looks for any page--custom_content_type.tpl.php page
+    $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
+  }
+}
+
